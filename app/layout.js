abd46600dedf331dd3a9b714b4ecf6a7f6bc1888
eonsyn <154956467+eonsyn@users.noreply.github.com>
2026-06-05 02:01:08 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -31,6 +31,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="a519RGXXnU8_HDFGvb_9NLkro6BAy_BnCXPq8fhFTkY" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PT4M8ZWW');
+        `}</Script>
+        {/* Google Analytics */}
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-WZL37ER1NV" strategy="afterInteractive" />
         <Script id="ga-init" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
@@ -40,6 +49,15 @@ export default function RootLayout({ children }) {
         `}</Script>
       </head>
       <body suppressHydrationWarning style={{ background: "#060910", minHeight: "100vh" }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT4M8ZWW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <HistoryProvider>
           <Navbar />
           <main style={{ minHeight: "100vh", paddingTop: "60px" }}>
